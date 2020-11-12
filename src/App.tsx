34296f4e9c38ebb6635f49e920d794c747c3f644
Adrian Szlegel <adrian.szlegel@hotmail.com>
2020-11-12 20:07:56 +0100

    project set up

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// https://opentdb.com/api.php?amount=10
+
+function App() {
+  return (
+    <div className="App">
+      Quiz
+    </div>
+  );
+}
+
+export default App;

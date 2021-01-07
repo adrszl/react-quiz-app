a1c710230e623ff735fbcd1e482ea67c33efbb3a
Adrian Szlegel <adrian.szlegel@hotmail.com>
2021-01-07 18:06:28 +0100

    new tags added & functions created for future needs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
+// COMPONENTS
+import QuestionCard from './components/QuestionCard';
+
 // https://opentdb.com/api.php?amount=10
 
-function App() {
+const App = () => {
+
+  const startTrivia = async () => {
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
   return (
     <div className="App">
-      Quiz
+      <h1>React Quiz App</h1>
+      <button className="start" onClick={ startTrivia }>START</button>
+      <p className="score">Score:</p>
+      <p className="">Loading questions...</p>
+      <QuestionCard />
+      <button className="next" onClick={ nextQuestion }>Next question</button>
     </div>
   );
 }

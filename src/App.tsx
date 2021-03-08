c301964733585b78f56483ee7428472098a611fa
Adrian Szlegel <adrian.szlegel@hotmail.com>
2021-03-08 23:29:51 +0100

    state and props added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // COMPONENTS
 import QuestionCard from './components/QuestionCard';
 
+const TOTAL_QUESTIONS = 10;
+
 // https://opentdb.com/api.php?amount=10
 
 const App = () => {
+
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
 
   const startTrivia = async () => {
 
@@ -25,7 +34,14 @@ const App = () => {
       <button className="start" onClick={ startTrivia }>START</button>
       <p className="score">Score:</p>
       <p className="">Loading questions...</p>
-      <QuestionCard />
+      <QuestionCard 
+        questionNr={ number + 1 }
+        totalQuestions={ TOTAL_QUESTIONS }
+        question={ questions[number].question }
+        answers={ questions[number].answers }
+        userAnswer={ userAnswers ? userAnswers[number] : undefined }
+        callback={ checkAnswer }
+      />
       <button className="next" onClick={ nextQuestion }>Next question</button>
     </div>
   );
